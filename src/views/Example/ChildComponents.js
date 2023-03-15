b9e1e4858a92c9557b4a1f6d2430f71ca74529e7
Van Quoc <quocx2003@gmail.com>
2023-03-15 19:12:25 +0700

    video13 stateless vs stateful

--- src/views/Example/ChildComponents.js
+++ src/views/Example/ChildComponents.js
@@ -1,33 +1,50 @@
 import React from 'react';
 
-class ChildComponent extends React.Component {
+// class ChildComponent extends React.Component {
 
 
+//     render() {
+//         console.log('check props: ', this.props)
+//         // let name = this.props.name;
+//         // let age = this.props.age;
 
-    render() {
-        console.log('check props: ', this.props)
-        // let name = this.props.name;
-        // let age = this.props.age;
+//         let { name, age, address, arrJobs } = this.props;
+//         return (
+//             <>
+//                 <div className='job-lists'>
+//                     {
+//                         arrJobs.map((item, index) => {
+//                             return (
+//                                 <div key={item.id}>
+//                                     {item.title} - {item.salary}
+//                                 </div>
+//                             )
+//                         })
+//                     }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
 
-        let { name, age, address, arrJobs } = this.props;
-        return (
-            <>
-                <div className='job-lists'>
-                    {
-                        arrJobs.map((item, index) => {
-                            return (
-                                <div key={item.id}> {item.title} - {item.salary} </div>
-                            )
-                        })
-                    }
-                </div>
+const ChildComponent = (props) => {
+    let { arrJobs } = props;
+    return (
+        <>
+            <div className='job-lists'>
+                {
+                    arrJobs.map((item, index) => {
+                        return (
+                            <div key={item.id}>
+                                {item.title} - {item.salary}
+                            </div>
+                        )
+                    })
+                }
+            </div>
 
-                <div>
-                    Child Component: {name} and {age} years old in {address}
-                </div>
-            </>
-        )
-    }
+        </>
+    )
 }
 
 export default ChildComponent;
