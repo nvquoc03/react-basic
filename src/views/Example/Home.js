3ccf1570ab4f5dfb3d0901d73a84b261ea9a3b4f
Van Quoc <quocx2003@gmail.com>
2023-03-18 10:39:25 +0700

    NavLink vs Redirect

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 class Home extends React.Component {
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
+
     render() {
         return (
             <div>Hello World From Home</div>
         )
     }
 }
-export default Home;
+export default withRouter(Home);
--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Nav.scss'
+import { NavLink } from "react-router-dom";
 
 class Nav extends React.Component {
     render() {
         return (
+
             <div className="topnav">
-                <a className="active" href="/">Home</a>
-                <a href="/todo">Todo</a>
-                <a href="/about">About</a>
+                <NavLink to="/" activeClassName='active' exact={true}>Home</NavLink>
+                <NavLink to="/todo" activeClassName='active'>Todos</NavLink>
+                <NavLink to="/about" activeClassName='active'>About</NavLink>
+
             </div>
         )
     }
